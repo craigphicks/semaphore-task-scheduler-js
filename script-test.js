25d1fe66add200866e204da72702b33730a793e7
craigphicks <13205732+craigphicks@users.noreply.github.com>
2020-12-13 23:55:47 -0800

    change from script-test.sh to script-test.js; added test-class-next-symbol.js

--- script-test.js
+++ script-test.js
@@ -4,13 +4,20 @@ var {AsyncIter}=require('./index');
 
 async function testone(cmd,aargs){
   return await new Promise((resolve,reject)=>{
-    let child=cp.spawn(cmd,aargs,{stdio:'ignore'});
+    try{
+      var child=cp.spawn(cmd,aargs,{stdio:'ignore'});
+    }catch(e){
+      reject(new Error(
+        `[spawn fail] `+
+        `${cmd},${JSON.stringify(aargs)}:: ${e.message}`));
+    }
     child.on('error',(e)=>{
       reject(`${cmd},${JSON.stringify(aargs)}:: ${e.message}`);
     });
     child.on('close',(code,signal)=>{
       if (code||signal)
-        reject(`${cmd},${JSON.stringify(aargs)}::FAIL ${code},${signal}`);
+        reject(new Error(
+          `${cmd},${JSON.stringify(aargs)}::FAIL ${code},${signal}`));
       resolve(`${cmd},${JSON.stringify(aargs)}::PASS ${code},${signal}`);
     });
   });
@@ -23,14 +30,15 @@ var progs=[
   "demo-next-symbol.js",
   "demo-wait-all.js",
   "test-class-async-iter.js",
+  "test-class-next-symbol.js",
 ];
 
 async function main(){
   let ts=new AsyncIter();
   for (let prog of progs)
-    ts.addTask(testone('node',[prog]));
+    ts.addTask(testone,'node',[prog]);
   ts.addEnd();
-  for (let iter of ts)
+  for await(let iter of ts)
     console.log(iter);
 }
 main()
---
+++ test-class-next-symbol.js
@@ -0,0 +1,28 @@
+'use strict';
+var {NextSymbol}=require('./index.js');
+var {task,/*makepr,snooze*/}=require('./demo-lib.js');
+async function testAsyncIter(){
+  let ts=new NextSymbol();
+  ts.addTask(task,0,11,false);
+  ts.addTask(task,1,10,false);
+  ts.addTask(task,2,10,true);
+  ts.addTask(task,3,10,true);
+  ts.addEnd();
+  let r=[];
+  out: for (;;){
+    switch (await ts.nextSymbol()){
+    case ts.symbolTaskResolved():
+      r.push(ts.getTaskResolvedValue());
+      break;
+    case ts.symbolTaskRejected():
+      r.push(ts.getTaskRejectedValue().message);
+      break;
+    case ts.symbolAllRead():
+      break out;
+    }
+  }
+  console.log(JSON.stringify(r,0,2));
+}
+testAsyncIter()
+  .then(()=>{console.log("success");process.exitCode=0;})
+  .catch((e)=>{console.log("failure: "+e.message);process.exitCode=1;});  
